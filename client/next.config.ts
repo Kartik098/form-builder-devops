713cb4eb0cfaa665c0e1e1252f5c55e6be98083a
Kartik <kartikgupta1805@gmail.com>
2025-06-26 17:44:32 +0530

    minor change

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+ allowedDevOrigins: ['http://54.145.126.112:3000'], 
   webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 1000,           // check for file changes every second
