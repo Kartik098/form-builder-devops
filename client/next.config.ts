a2b3a3d4607ea7d839eeacf6caea6dc2fabb3492
Kartik <kartikgupta1805@gmail.com>
2025-06-26 20:34:08 +0530

    commented changes

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,3 +6,18 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//  allowedDevOrigins: ['http://54.145.126.112'],
+//   webpackDevMiddleware: config => {
+//     config.watchOptions = {
+//       poll: 1000,           // check for file changes every second
+//       aggregateTimeout: 300 // delay before rebuilding
+//     };
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
